2839edc1327c2c95d846698750cda895a588fd5e
Yun Yang & Yingying Chen <yunyang@thoughtworks.com>
2012-07-03 10:57:44 +0800

    initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AshokaPrototype::Application.config.secret_token = '24965cf0eecad0959092d88ba87f70c5c62ba125ea6f9deb4057bf14338c59371e7da7293860b788d1894af0129fff7ffda7df3e865410f14547d1a165d53ea7'
